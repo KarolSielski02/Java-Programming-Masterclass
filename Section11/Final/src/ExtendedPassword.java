7f4c86dd13f32f4a191d312729cef29259676c52
KarolSielski02 <mrkarlos2002@gmail.com>
2022-11-30 23:05:14 +0100

    com

---
+++ Section11/Final/src/ExtendedPassword.java
@@ -0,0 +1,8 @@
+//public class ExtendedPassword{
+//    private int decryptedPassword;
+//
+//    public ExtendedPassword(int password) {
+//        super(password);
+//        this.decryptedPassword = password;
+//    }
+//}
